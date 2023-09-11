dd36bf2bc12a34e5c771daddab42d7757e38c1e6
elhanAliev <alievelhan90@gmail.com>
2023-09-11 16:49:17 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,129 @@
+const ROOT = document.getElementById('root')
+
+const btnModal = document.createElement('button')
+ROOT.append(btnModal)
+btnModal.textContent = 'SHOW MODAL'
+
+btnModal.style.cssText = `
+background-color: azure;
+    color:black ;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 10px;
+    width: 150px;
+    height: 50px;
+`;
+
+btnModal.addEventListener('mouseover', () => {
+    btnModal.style.backgroundColor = '#00000070'
+})
+ 
+
+const modal = document.createElement('div')
+const modalWindow = document.createElement('div') 
+ modalWindow.style.backgroundColor = 'red';
+ modalWindow.style.backgroundColor = 'green';
+modal.style.cssText = `
+position: fixed;
+   top: 0;
+   left: 0;
+   width: 100vw;
+   height: 100vw;
+   background-color: #00000060;
+   display: flex;
+   align-items: center;
+   justify-content: center;`
+
+
+modalWindow.style.cssText = `
+ 
+
+   width: 400px;
+   height:200px;
+   background-color: #fff;
+   position: fixed;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   padding: 20px;
+   font-size: 18px;
+   color: black;
+   border-radius:8px;
+   border-color:gray;
+   `
+
+   
+modalWindow.textContent = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe omnis, impedit aliquid enim fugit dicta natus provident aut incidunt optio, facilis dolor pariatur aspernatur maxime consequatur neque voluptatum obcaecati nemo!
+`
+modal.append(modalWindow)
+
+btnModal.addEventListener('click', () =>{
+    ROOT.append(modal)
+})
+
+
+const btnClose = document.createElement('button')
+btnClose.textContent = 'x'
+modalWindow.append(btnClose)
+
+btnClose.style.cssText = `
+   position: absolute;
+   right: 9px;
+   top: 9px;
+   background-color: white;
+   border: none;
+   width: 30px;
+   height: 30px;
+   font-size: 30px;
+   cursor: pointer;`
+modalWindow.append(btnClose)
+
+btnClose.addEventListener('click', () =>{
+    modal.remove(modal)
+})
+
+const btnOpen = document.createElement('button')
+btnOpen.textContent = 'ACCERT'
+modalWindow.append(btnOpen)
+
+btnOpen.style.cssText = `
+   position: absolute;
+   left: 24px;
+   top:156px;
+   background-color: green;
+   color:white;
+   border-radius:8px;
+   width: 72px;
+   height: 27px;
+   font-size: 12px;
+   cursor: pointer;`
+
+   const btnOpen11 = document.createElement('button')
+btnOpen11.textContent = 'DECLINE'
+modalWindow.append(btnOpen11)
+
+btnOpen11.style.cssText = `
+position: absolute;
+   left:100px;
+   top:156px;
+   background-color: red;
+   color:white;
+   border-radius:8px;
+   width: 72px;
+   height: 27px;
+   font-size: 12px;
+   cursor: pointer;`
+
+   btnOpen.addEventListener('click', () =>{
+    modalWindow.style.backgroundColor = 'red';
+    btnClose.style.backgroundColor = 'red';
+
+})
+btnOpen11.addEventListener('click', () =>{
+    modalWindow.style.backgroundColor = 'green';
+    btnClose.style.backgroundColor = 'green';
+})
+
+
+const message = prompt('HELLO! What is your name?')
+alert (`${message} welcome to my web-page🙃😉`)
